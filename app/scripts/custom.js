fedb537a7204d87e42931a2e212ee6a48878849e
William <willyk99@gmail.com>
2014-06-17 15:41:03 +0300

    WM: Main navigation active states for the selected location

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -2,3 +2,10 @@ function customShowTab(e){
     e.preventDefault();
     $(this).tab('show');
 }
+
+var navlinks=$("#ctr_nav li");
+
+navlinks.click(function(e){
+	navlinks.removeClass("active");
+	$(this).addClass("active");
+});
