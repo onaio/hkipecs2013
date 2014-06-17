547f77f7bef2f9fc798d583f7365de16cec577ad
Ukang'a Dickson <ukanga@gmail.com>
2014-06-17 13:15:19 +0300

    UK: WM: grunt build

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,14 +1,4 @@
-(function(){
-
-    console.log('here');
-    $(document).ready(function(){
-        console.log('here again');
-        k = $('#summarytabs');
-        $('#summarytabs a').click(function (e) {
-            console.log('here clicked');
-            e.preventDefault();
-            $(this).tab('show');
-        });
-    });
-
-})();
+function customShowTab(e){
+    e.preventDefault();
+    $(this).tab('show');
+}
