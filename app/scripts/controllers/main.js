1c6645892350a3df82275fc644ba88ce36e947ff
William <willyk99@gmail.com>
2014-06-18 14:44:18 +0300

    WM: Moved chart test to new branch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,36 +14,7 @@
 
         $scope.survey = {};
         $scope.country = "Nigeria";
-		var setChartData = function(data){
-	        for(var lp=0; lp<data.length; lp++){
-	        	intDays.push(data[lp].date_interview);
-	        	intCount.push(data[lp].count);
-	        }
-	        
-	        //Chart config
-	        $scope.config = {
-			    title: 'Survey by Interview date',
-				tooltips: true,
-			    labels: true,
-			    mouseover: function() {},
-			    mouseout: function() {},
-			    click: function() {},
-			    legend: {
-			      display: true,
-			      position: 'right'
-			    }
-			  };
-			
-			  $scope.data = {
-			    series: ["Date of Interview"],
-			    data: [
-				    {
-				      x: intDays,
-				      y: intCount //[0,1,2,3,4,5]
-				    }
-			    ]
-			  };
-		};
+        
         var query = {
             group: 'consent_group/date_interview',
             user: 'hkinigeriadata',
@@ -51,12 +22,8 @@
             site: 'ona.io'
         };
         query.name = 'date_interview';
-        var interviewbyDate = ona.query(query, setChartData);
+        var interviewbyDate = ona.query(query);
         $scope.date_interview_data = interviewbyDate;
-        var intDays = [];
-        var intCount = [];
-        
-        console.log("Data: "+interviewbyDate);
         
         
         query.group = 'consent_group/interviewer_name';
