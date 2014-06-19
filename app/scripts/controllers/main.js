6e1297493bc315b219e069a8b4561bc61faf9bb0
William <willyk99@gmail.com>
2014-06-19 15:54:00 +0300

    WM: Reformat main.js script

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,12 @@
 /*global console, angular */
-(function(){
-    'use strict';
+(function() {'use strict';
 
     var pecsApp = angular.module('hkipecsApp');
 
-    pecsApp.controller('nigeriaCtrl', [ "$scope", "$http", "$log", "OnadataService", function ($scope, $http, $log, ona) {
-		$scope.$log = $log;
-		
+    pecsApp.controller('nigeriaCtrl', ["$scope", "$http", "$log", "OnadataService",
+    function($scope, $http, $log, ona) {
+        $scope.$log = $log;
+
         $http.defaults.useXDomain = true;
         delete $http.defaults.headers.common['X-Requested-With'];
         // onadata api token
@@ -14,51 +14,84 @@
 
         $scope.survey = {};
         $scope.country = "Nigeria";
-        
+
         var query = {
-            group: 'consent_group/date_interview',
-            user: 'hkinigeriadata',
-            formid: 'caretaker_Akwa_IBOMM',
-            site: 'ona.io'
+            group : 'consent_group/date_interview',
+            user : 'hkinigeriadata',
+            formid : 'caretaker_Akwa_IBOMM',
+            site : 'ona.io'
         };
         query.name = 'date_interview';
         var interviewbyDate = ona.query(query);
         $scope.date_interview_data = interviewbyDate;
-        $scope.interviewDateTable = { 
-	        		data: 'date_interview_data',
-	        		columnDefs: [{field:'date_interview', displayName:'Date of Interview',cellFilter:'date'}, 
-	        					{field:'count', displayName:'No. of Surveys'}],
-	        		sortInfo: {fields:['date_interview'], directions:['asc']}
-        		};
-        
+        $scope.interviewDateTable = {
+            data : 'date_interview_data',
+            columnDefs : [{
+                field : 'date_interview',
+                displayName : 'Date of Interview',
+                cellFilter : 'date'
+            }, {
+                field : 'count',
+                displayName : 'No. of Surveys'
+            }],
+            sortInfo : {
+                fields : ['date_interview'],
+                directions : ['asc']
+            }
+        };
+
         query.group = 'consent_group/interviewer_name';
         query.name = 'interviewer_name';
         $scope.interviewer_data = ona.query(query);
-        $scope.interviewerTable = { 
-	        		data: 'interviewer_data',
-	        		columnDefs: [{field:'interviewer_name', displayName:'Interviewer'}, 
-	        					{field:'count', displayName:'No. of Surveys'}],
-	        		sortInfo: {fields:['interviewer_name'], directions:['asc']}
-        		};
+        $scope.interviewerTable = {
+            data : 'interviewer_data',
+            columnDefs : [{
+                field : 'interviewer_name',
+                displayName : 'Interviewer'
+            }, {
+                field : 'count',
+                displayName : 'No. of Surveys'
+            }],
+            sortInfo : {
+                fields : ['interviewer_name'],
+                directions : ['asc']
+            }
+        };
 
         query.group = 'consent_group/village';
         query.name = 'village';
         $scope.village_data = ona.query(query);
-        $scope.villageTable = { 
-	        		data: 'village_data',
-	        		columnDefs: [{field:'village', displayName:'Village'}, 
-	        					{field:'count', displayName:'No. of Surveys'}],
-	        		sortInfo: {fields:['village'], directions:['asc']}
-        		};
+        $scope.villageTable = {
+            data : 'village_data',
+            columnDefs : [{
+                field : 'village',
+                displayName : 'Village'
+            }, {
+                field : 'count',
+                displayName : 'No. of Surveys'
+            }],
+            sortInfo : {
+                fields : ['village'],
+                directions : ['asc']
+            }
+        };
 
         query.group = 'consent_group/village_LGA';
         query.name = 'ward';
         $scope.wards = ona.query(query);
-        $scope.wardTable = { 
-	        		data: 'wards',
-	        		columnDefs: [{field:'ward', displayName:'Ward'}, 
-	        					{field:'count', displayName:'No. of Surveys'}],
-	        		sortInfo: {fields:['ward'], directions:['asc']}
-        		};
+        $scope.wardTable = {
+            data : 'wards',
+            columnDefs : [{
+                field : 'ward',
+                displayName : 'Ward'
+            }, {
+                field : 'count',
+                displayName : 'No. of Surveys'
+            }],
+            sortInfo : {
+                fields : ['ward'],
+                directions : ['asc']
+            }
+        };
     }]);
 })();
