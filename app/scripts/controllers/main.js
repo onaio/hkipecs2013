bd20e40a5bc99941b92aaefb6aaee1337e01deb8
William Mucheru <willyk99@gmail.com>
2014-06-25 17:55:24 +0300

    WM: Collect data from all villages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -61,10 +61,19 @@
                 directions : ['asc']
             }
         };
-
-        query.group = 'given_consent/area';
-        query.name = 'village';
-        $scope.village_data = ona.query(query);
+        
+        var village_groups = ["abakaliki","afikpo_north","afikpo_south","ebonyi","ezza_north",
+                            "ezza_south","ikwo","ishielu","ivo","izzi","ohaozara","ohaukwu","onicha"];
+        var obj_array = {};
+        
+        for(var g=0; g<village_groups.length; g++){
+            query.group = 'given_consent/'+village_groups[g];
+            query.name = 'village';
+            $scope.village_data = ona.query(query);
+            
+            console.log($scope.village_data);
+        }
+        
         $scope.villageTable = {
             data : 'village_data',
             columnDefs : [{
