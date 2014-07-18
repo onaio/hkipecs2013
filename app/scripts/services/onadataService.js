db2da6abb12ab495250bc5f5050b44bb342d5d26
Ukang'a Dickson <ukanga@gmail.com>
2014-07-19 00:37:13 +0300

    UK: new api

--- app/scripts/services/onadataService.js
+++ app/scripts/services/onadataService.js
@@ -5,8 +5,8 @@
     angular.module('hkipecsApp')
       .service('OnadataService', ["$resource", function Onadataservice($resource) {
           return $resource(
-              'https://:site/api/v1/stats/submissions/:user/:formid',
-              {user: 'demouser', formid: 'formid', site: 'ona.io'},
+              'https://:site/api/v1/stats/submissions/:form_pk',
+              {user: 'demouser', form_pk: '123', site: 'ona.io'},
               {query: {method: 'GET', params: {group: 'enqueteur'}, withCredentials: true, isArray: true}}
           );
       }]);
